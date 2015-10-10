64be3f8f4fe32fbdf686dbfefa2813c209e71926
Ryan Lee <Ryan@calvisitor-10-105-151-53.CalVisitor.1918.Berkeley.EDU>
2015-10-10 02:39:29 -0700

    Main storyboard updated

---
+++ Unit/Team.swift
@@ -0,0 +1,15 @@
+//
+//  Team.swift
+//  Unit
+//
+//  Created by Ryan Lee on 10/10/15.
+//  Copyright © 2015 TheDRE. All rights reserved.
+//
+
+import Foundation
+import Parse
+import Bolts
+
+class Team {
+    
+}
--- Unit/User.swift
+++ Unit/User.swift
@@ -7,7 +7,20 @@
 //
 
 import Foundation
+import Parse
+import Bolts
 
 class User {
+    let email : String!
+    let password : String!
+    let name : String!
+    
+    // Constructs a User object
+    init(user_email : String, user_password : String, user_name : String) {
+        email = user_email
+        password = user_password
+        name = user_name
+    }
+    
     
 }
