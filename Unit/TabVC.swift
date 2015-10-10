8a2f8f151eda10069af573b5dbe87ad746915d25
Eric Tran <Eric@calvisitor-10-105-156-99.CalVisitor.1918.Berkeley.EDU>
2015-10-10 04:03:26 -0700

    Initial UI layout

---
+++ Unit/TabVC.swift
@@ -0,0 +1,14 @@
+//
+//  TabVC.swift
+//  Unit
+//
+//  Created by Eric Tran on 10/10/15.
+//  Copyright © 2015 TheDRE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TabVC: UITabBarController {
+    
+}
