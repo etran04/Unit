afaea3965c55793c34057532138bd42283712cb0
Ryan Lee <ryanyunglee@gmail.com>
2015-10-10 09:52:22 -0700

    Adds tasks successfully

--- Unit/AddTaskViewController.swift
+++ Unit/AddTaskViewController.swift
@@ -16,39 +16,48 @@ class AddTaskViewController : UIViewController {
     @IBOutlet weak var taskDescription: UITextField!
     @IBOutlet weak var priorityLevel: UISegmentedControl!
     
-    @IBAction func buttonPressed(sender: AnyObject) {
-        //let user = PFUser.currentUser()
-        let teamId : String = "Zqz4CaACw2"
-        //let teamId : String = user["currentTeam"] as! String
-        let newTask = PFObject(className: "Task")
-        //TODO - access current team to get teamID. store locally to use in getObjectInBackgroundWithId
-        newTask["teamId"] = teamId
-        //newTask["description"] = taskDescription.text
-        //newTask["priorityLevel"] = priorityLevel.selectedSegmentIndex
-        newTask.saveInBackgroundWithBlock {
-            (success : Bool, error : NSError?) -> Void in
-            if (success) {
-                let query = PFQuery(className:"Team")
-                query.getObjectInBackgroundWithId(teamId) {
-                    (team: PFObject?, error: NSError?) -> Void in
-                    if error == nil && team != nil {
-                        team!.addObject(newTask, forKey: "Tasks")
-                        team!.saveInBackgroundWithBlock {
-                            (success : Bool, error : NSError?) -> Void in
-                            if (success) {
-                                //TODO - close New Task screen and go back to main task screen
+    @IBAction func addTaskButtonPressed(sender: AnyObject) {
+        let user = PFUser.currentUser()
+        
+        user?.fetchInBackgroundWithBlock({(object: PFObject?, error: NSError?) -> Void in
+            let teamId = object!.objectForKey("currentTeam") as! String
+            
+            let newTask = PFObject(className: "Task")
+            //TODO - access current team to get teamID. store locally to use in getObjectInBackgroundWithId
+            newTask["teamId"] = teamId
+            newTask["description"] = self.taskDescription.text
+            newTask["priorityLevel"] = self.priorityLevel.selectedSegmentIndex
+            newTask.saveInBackgroundWithBlock {
+                (success : Bool, error : NSError?) -> Void in
+                if (success) {
+                    let query = PFQuery(className:"Team")
+                    query.getObjectInBackgroundWithId(teamId) {
+                        (team: PFObject?, error: NSError?) -> Void in
+                        if error == nil && team != nil {
+                            team!.addObject(newTask, forKey: "Tasks")
+                            team!.saveInBackgroundWithBlock {
+                                (success : Bool, error : NSError?) -> Void in
+                                if (success) {
+                                    //TODO - close New Task screen and go back to main task screen
+                                }
+                                else {
+                                    print("SUCKER")
+                                }
                             }
-                            else {
-                                print("SUCKER")
-                            }
+                        } else {
+                            print(error)
                         }
-                    } else {
-                        print(error)
                     }
+                } else {
+                    print("YOU SUCK")
                 }
-            } else {
-                print("YOU SUCK")
             }
-        }
+            
+            
+            
+        })
+        
+//        let teamId : String =
+        
     }
 }
