84c50858770ba8ad5982f0a35bd8cc5d1ce24ceb
Ryan Lee <Ryan@calvisitor-10-105-151-53.CalVisitor.1918.Berkeley.EDU>
2015-10-10 04:13:27 -0700

    Added ParseSignUpVC

---
+++ Unit/ParseSignUpVC.swift
@@ -0,0 +1,27 @@
+//
+//  ParseSignUpVC.swift
+//  Unit
+//
+//  Created by Ryan Lee on 10/10/15.
+//  Copyright (c) 2015 The DRE. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+
+class ParseSignUpVC: PFSignUpViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+
+}
