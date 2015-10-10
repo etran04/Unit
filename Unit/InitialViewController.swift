0fbbde177c951accf5f80dfcb5d39d164de588ef
Ryan Lee <ryanyunglee@gmail.com>
2015-10-10 05:02:46 -0700

    Deleted unnecessary lines

--- Unit/InitialViewController.swift
+++ Unit/InitialViewController.swift
@@ -40,7 +40,6 @@ class InitialViewController: UIViewController, PFLogInViewControllerDelegate, PF
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     func logInViewController(controller: PFLogInViewController, didLogInUser user: PFUser) -> Void {
